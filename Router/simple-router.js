d5463578025ce8a72345c9c412bedda15e88cef3
Mohsen Karimi <Mohsen Karimi>
2017-03-09 23:36:05 +0330

    changes

--- Router/simple-router.js
+++ Router/simple-router.js
@@ -1,44 +1,27 @@
 class SimpleRouter {
-    constructor(location) {
+    constructor(location, window) {
         this.routes = [];
         this.location = location;
         this.root = '/';
 
-        window.onpopstate = function (event) {
-            console.log('event' + event);
-        };
+        window.addEventListener('popstate', function (event) {
+            if (event.state) {
+                console.log('event' + event);
+            }
+        }, false);
     }
 
-    getFragment() {
-        let fragment = '';
-        fragment = this.clearSlashes(decodeURI(this.location.pathname + this.location.search));
-        fragment = fragment.replace(/\?(.*)$/, '');
-        fragment = this.root != '/' ? fragment.replace(this.root, '') : fragment;
-
-        return this.clearSlashes(fragment);
-    }
-
-    clearSlashes(path) {
+    _clearSlashes(path) {
         return path.toString().replace(/\/$/, '').replace(/^\//, '');
     }
 
-    add(re, handler) {
-        if (typeof re == 'function') {
-            handler = re;
-            re = '';
-        }
-        this.routes.push({ re: re, handler: handler });
-        return this;
-    }
+    _getFragment() {
+        let fragment = '';
+        fragment = this._clearSlashes(decodeURI(location.pathname + location.search));
+        fragment = fragment.replace(/\?(.*)$/, '');
+        fragment = this.root != '/' ? fragment.replace(this.root, '') : fragment;
 
-    remove(param) {
-        for (var i = 0, r; i < this.routes.length, r = this.routes[i]; i++) {
-            if (r.handler === param || r.re.toString() === param.toString()) {
-                this.routes.splice(i, 1);
-                return this;
-            }
-        }
-        return this;
+        return this._clearSlashes(fragment);
     }
 
     flush() {
@@ -47,10 +30,29 @@ class SimpleRouter {
         return this;
     }
 
+    add(URI, handler) {
+        if (typeof URI == 'function') {
+            handler = URI;
+            URI = '';
+        }
+        this.routes.push({ URI: URI, handler: handler });
+        return this;
+    }
+
+    remove(param) {
+        for (var i = 0, r; i < this.routes.length, r = this.routes[i]; i++) {
+            if (r.handler === param || r.URI.toString() === param.toString()) {
+                this.routes.splice(i, 1);
+                return this;
+            }
+        }
+        return this;
+    }
+
     check(f) {
-        var fragment = f || this.getFragment();
+        var fragment = f || this._getFragment();
         for (var i = 0; i < this.routes.length; i++) {
-            var match = fragment.match(this.routes[i].re);
+            var match = fragment.match(this.routes[i].URI + '(?:\/|$)');
             if (match) {
                 match.shift();
                 this.routes[i].handler.apply({}, match);
@@ -62,21 +64,21 @@ class SimpleRouter {
 
     listen() {
         var self = this;
-        var current = self.getFragment();
+        var current = self._getFragment();
         var fn = function () {
-            if (current !== self.getFragment()) {
-                current = self.getFragment();
+            if (current !== self._getFragment()) {
+                current = self._getFragment();
                 self.check(current);
             }
         }
         clearInterval(this.interval);
-        this.interval = setInterval(fn, 50);
+        this.interval = setInterval(fn, 150);
         return this;
     }
 
     navigate(path) {
         path = path ? path : '';
-        history.pushState(null, null, this.root + this.clearSlashes(path));
+        history.pushState(null, null, this.root + this._clearSlashes(path));
         return this;
     }
 }
